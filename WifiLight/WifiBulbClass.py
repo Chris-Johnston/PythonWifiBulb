db2458897a2ccd532e047660807a3b86db088d73
Chris Johnston <chris@thejohnstons.net>
2016-10-12 09:22:22 -0700

    cleaned up code

--- WifiLight/WifiBulbClass.py
+++ WifiLight/WifiBulbClass.py
@@ -5,7 +5,6 @@ class WifiBulb(object):
     """class for controlling a wifi lightbulb"""
     mode = "31" # default mode
     magicBytes = "00f00f" # have yet to investigate what these do
-
     PORT = 5577
 
     def __init__(self, IP):
@@ -32,11 +31,11 @@ class WifiBulb(object):
         print("Sending color: " + str(color))
         # the structure of the packets sent to the light are
         # pattern, red, green, blue, "00f00f", some 1 byte checksum?
+        # this is a work in progress. planned to use the different patterns
         # I have yet to establish a pattern between the colors and the checksum
         # but since it's only 255 bytes, I don't think it's too big of a deal to just go with it for now
         for x in range(255):
             message = WifiBulb.mode + format(color[0], "02x") + format(color[1], "02x") + format(color[2], "02x") + WifiBulb.magicBytes + format(x, "02x")
-            #print(message)
             try:
                 self.s.send(bytes.fromhex(message))
             except:
