edc7c750ef74425a93f40039b07a0ff47446563a
Chris Johnston <chris@thejohnstons.net>
2016-10-12 22:16:16 -0700

    Added checksum calculation which results in increased speed for setting color

--- WifiLight/WifiBulbClass.py
+++ WifiLight/WifiBulbClass.py
@@ -1,6 +1,15 @@
 import socket
 import binascii
 
+def getChecksumValue(byteArray):
+        """ returns the checksum value as a byte from an array of bytes """
+        #sum all the values
+        sumOfValues = 0
+        for x in range(len(byteArray)):
+            sumOfValues += byteArray[x]
+        # return 
+        return sumOfValues % pow(2, len(byteArray) + 1)
+
 class WifiBulb(object):
     """class for controlling a wifi lightbulb"""
     mode = "31" # default mode
@@ -10,6 +19,7 @@ class WifiBulb(object):
     def __init__(self, IP):
         """ constructor with IP address """
         self.IP = IP
+        self.useDebug = False
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
@@ -34,10 +44,15 @@ class WifiBulb(object):
         # this is a work in progress. planned to use the different patterns
         # I have yet to establish a pattern between the colors and the checksum
         # but since it's only 255 bytes, I don't think it's too big of a deal to just go with it for now
+
+        # attempting to generate checksums
         for x in range(255):
-            message = WifiBulb.mode + format(color[0], "02x") + format(color[1], "02x") + format(color[2], "02x") + WifiBulb.magicBytes + format(x, "02x")
+            # mode + red + green + blue + magicBytes + checksum
+            message = WifiBulb.mode + format(color[0], "02x") + format(color[1], "02x") + format(color[2], "02x") + WifiBulb.magicBytes 
+            messageBytes = bytearray.fromhex(message)
+            checksum = getChecksumValue(messageBytes)
+            messageBytes.append(checksum)
             try:
-                self.s.send(bytes.fromhex(message))
+                self.s.send(messageBytes)
             except:
                 print("Failed to set color")
-                #break
