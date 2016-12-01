db148419ff5475912c61de61d2b7acba6427a0af
Chris Johnston <allcapson@gmail.com>
2016-12-01 13:44:06 -0800

    Renamed files and added CLI for WifiBulb. Also updated readme

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-# WifiLedClass
-A python class for controlling "MagicLight Wifi Smart LED Light Bulbs". Standalone from the Android or iOS app.
+# PythonWifiLedBulbController
+A python script and class for controlling "MagicLight Wifi Smart LED Light Bulbs". Standalone from the Android or iOS app.
 
 ## Usage
 This requires that your bulb is connected to your local network, and you have it's IP address. You can obtain the IP by looking in the app, or using NMAP to search for port 5577.
 
-A test usage can be seen in [WifiBulb.py](WifiLight/WifiBulb.py), however here's a quick snippet:
+### CLI
+
+Bulb color can be set from the command line as well. Ensure that `python` is python 3 not python 2. Tested in windows command line and Bash shell.
+
 ```
+python3 WifiBulb.py 192.168.x.x 255 0 0
+```
+
+### In code
+
+A test usage can be seen in [testProgram.py](WifiLight/testProgram.py), however here's a quick snippet:
+```python
 # import the class
-from WifiBulbClass import WifiBulb
+from WifiBulb import WifiBulb
 
 # create a new object and assign it's IP address
 IP = "192.168.x.x"
@@ -26,3 +36,16 @@ myLightBulb.disconnect()
 ## What type of Wifi enabled lightbulbs work?
 So far I've used this with these bulbs: [Magic Light Wifi Smart Light Bulb](https://www.amazon.com/MagicLight-WiFi-Smart-Light-Bulb/dp/B00SIDVZSW).
 I have used two versions of these bulbs, but they both worked with the same Android app. They list themselves as v5 and v7.
+
+## Notes on packet structure
+
+A while back I used Wireshark to view some packets going to the lights and managed to get this working.
+Here's a sample of what the data in a packet may look like: `31FF00FF00F00FXX`
+
+- '31' is the "mode" of the bulb. I noticed that when I changed to a different mode, this value would change. This is the only mode I've tested so far.
+
+- 'FF00FF' is the color. In this case #FF00FF (purple).
+
+- '00F00F' are some "magic bytes". I don't know what they do, but they never changed in this pattern, so I wasn't concerned about it.
+
+- 'XX' is the checksum value.
--- WifiLight/WifiBulb.py
+++ WifiLight/WifiBulb.py
@@ -1,29 +1,65 @@
-import time
-from WifiBulbClass import WifiBulb
+﻿import socket
+import sys
+import binascii
 
-# create a couple of WifiBulbs
-light1 = WifiBulb("192.168.2.40")
-light2 = WifiBulb("192.168.2.37")
-light3 = WifiBulb("192.168.2.44")
+def getChecksumValue(byteArray):
+        """ returns the checksum value as a byte from an array of bytes """
+        #sum all the values
+        sumOfValues = 0
+        for x in range(len(byteArray)):
+            sumOfValues += byteArray[x]
+        # return 
+        return sumOfValues % pow(2, len(byteArray) + 1)
 
-# connect
-light1.connect()
-light2.connect()
-light3.connect()
+class WifiBulb(object):
+    """class for controlling a wifi lightbulb"""
+    mode = "31" # default mode
+    magicBytes = "00f00f" # have yet to investigate what these do
+    PORT = 5577
 
-# set color according to (R, G, B)
-light1.setColor((255,0,0))
-light2.setColor((0,255,0))
-light3.setColor((0,0,255))
+    def __init__(self, IP):
+        """ constructor with IP address """
+        self.IP = IP
+        self.useDebug = False
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-time.sleep(5)
+    def connect(self):
+        """ connects the socket """
+        print ("Connecting to : " + self.IP + " : " + str(WifiBulb.PORT) )
+        try:
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s.connect((self.IP, WifiBulb.PORT))
+            print ("Connected to " + self.IP + ":" + str(WifiBulb.PORT))
+        except:
+            print ("failed to connect")
 
-light1.setColor((0,0,0))
-light2.setColor((0,0,0))
-light3.setColor((0,0,0))
+    def disconnect(self):
+        """ disconnects the socket """
+        self.s.detach()
 
-time.sleep(5)
+    def setColor(self, color):
+        """sets the color the given tuple in the format (R, G, B)"""
+        print("Sending color: " + str(color))
+                
+        # mode + red + green + blue + magicBytes + checksum
+        message = WifiBulb.mode + format(color[0], "02x") + format(color[1], "02x") + format(color[2], "02x") + WifiBulb.magicBytes 
+        messageBytes = bytearray.fromhex(message)
+        checksum = getChecksumValue(messageBytes)
+        messageBytes.append(checksum)
+        try:
+            self.s.send(messageBytes)
+        except:
+            print("Failed to set color")
 
-light1.disconnect()
-light2.disconnect()
-light3.disconnect()
+# allow CLI for setting colors
+#python3 WifiBulb.py <IP> <R> <G> <B>
+if __name__ == "__main__":
+    # create a bulb
+    bulb = WifiBulb(sys.argv[1])
+    # connect
+    bulb.connect()
+    # setColor
+    color = (int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+    bulb.setColor(color)
+    # disconnect
+    bulb.disconnect()
